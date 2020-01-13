d7ef299ff73b8070b75559570e427e080aaca63b
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2020-01-13 02:28:05 +0000

    a

--- SSM___/a.sh
+++ SSM___/a.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SSM15 --s_dim 64 512 --comment trans_share-min_stddev_a-s_trans
+CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SSM15 --s_dim 64 128 --comment trans_share-min_stddev_a-s_trans
+# CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SSM15 --s_dim 64 512 --comment trans_share-min_stddev_a-s_trans
 # CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SimpleSSM --s_dim 1024 --comment trans_share-min_stddev
 # CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SSM13 --s_dim 256 1024 --comment hie
 # CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=16 python train.py --model SSM14 --s_dim 256 512 --comment res
