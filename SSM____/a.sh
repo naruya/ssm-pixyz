68aedfca81d35847865930070f4e20ff81ecaa56
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2020-01-20 19:22:44 +0000

    fix sh

--- SSM____/a.sh
+++ SSM____/a.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=16 python train.py --model SSM16 --s_dim 64 1024 1024 1024 --comment beta1_std_kl01-7 --min_stddev 1e-7
+CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=16 python train.py --model SSM16 --s_dim 64 1024 1024 1024 --comment beta1_std_kl01 --min_stddev 1e-7
 # CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=16 python train.py --model SSM16 --s_dim 64 1024 --resume --resume_name Jan19_19-06-23_SSM16_s64-1024_9bafb17_base --resume_time Jan20_15-00-28 --resume_itr 270400 --resume_epoch 201
 # CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=16 python train.py --model SSM16 --s_dim 64 --comment base
 # CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=16 python train.py --model SimpleSSM --s_dim 64 --comment base_kl01_5_no-std_no-share
