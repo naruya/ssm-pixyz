51a9550b26d256d542b2c34872fd4c67c5f05564
Naruya Kondo <n-kondo@weblab.t.u-tokyo.ac.jp>
2020-05-03 18:48:45 +0000

    fix x_0 and x indicies

--- SSM/model.py
+++ SSM/model.py
@@ -21,6 +21,13 @@ class Base(nn.Module):
     def forward(self):
         raise NotImplementedError
 
+    def forward_(self, x_0, x, a, train):
+        if train:
+            loss, info = self.train_(x_0, x, a)
+        else:
+            loss, info = self.test_(x_0, x, a)
+        return loss, info
+
     def train_(self, x_0, x, a):
         self.train()
         self.optimizer.zero_grad()
@@ -63,8 +70,10 @@ class SSM(Base):
             s_dim = self.s_dims[i]
             a_dims = [self.a_dim] + self.s_dims[i-1:i]  # use s_dims[i-1]
 
-            prior = Prior(i, s_dim, a_dims, self.min_stddev).to(self.device)
-            posterior = Posterior(i, s_dim, self.h_dim, a_dims, self.min_stddev).to(self.device)
+            prior = Prior(i, s_dim, a_dims,
+                          self.min_stddev).to(self.device)
+            posterior = Posterior(i, s_dim, self.h_dim, a_dims,
+                                  self.min_stddev).to(self.device)
             if args.resnet:
                 encoder = ResEncoder(i).to(self.device)
                 decoder = ResDecoder(i, s_dim, self.device).to(self.device)
@@ -73,10 +82,14 @@ class SSM(Base):
                 decoder = Decoder(i, s_dim, self.device).to(self.device)
 
             if len(args.device_ids) > 0:
-                prior = nn.DataParallel(prior, device_ids=args.device_ids)
-                posterior = nn.DataParallel(posterior, device_ids=args.device_ids)
-                encoder = nn.DataParallel(encoder, device_ids=args.device_ids)
-                decoder = nn.DataParallel(decoder, device_ids=args.device_ids)
+                prior = nn.DataParallel(prior,
+                                        device_ids=args.device_ids)
+                posterior = nn.DataParallel(posterior,
+                                            device_ids=args.device_ids)
+                encoder = nn.DataParallel(encoder,
+                                          device_ids=args.device_ids)
+                decoder = nn.DataParallel(decoder,
+                                          device_ids=args.device_ids)
 
             self.priors.append(prior)
             self.posteriors.append(posterior)
@@ -118,6 +131,8 @@ class SSM(Base):
 
         keys = set(locals().keys()) - keys - {"keys"}
 
+        x = x.transpose(0, 1).to(self.device)  # T,B,3,28,28
+        a = a.transpose(0, 1).to(self.device)  # T,B,4
         _T, _B = x.size(0), x.size(1)
         _x = []
 
@@ -130,11 +145,12 @@ class SSM(Base):
             for i in range(self.num_states):
                 h_t = self.encoders[i](x_t)
                 s_prev = s_prevs[i]
-                a_list = [a_t] + s_ts[-1:]  # use one hierarchy upper state
+                a_list = [a_t] + s_ts[-1:]  # use upper state
 
                 q = Normal(*self.posteriors[i](s_prev, h_t, a_list))
                 p = Normal(*self.priors[i](s_prev, a_list))
-                s_losss[i] = torch.sum(kl_divergence(q, p), dim=[1]).mean()  # (q,p)! Not (p,q)!
+                # KL(q,p)! Not KL(p,q)!
+                s_losss[i] = torch.sum(kl_divergence(q, p), dim=[1]).mean()
                 s_t = p.mean if prior_sample else q.rsample()
                 s_ts.append(s_t)
 
@@ -147,17 +163,22 @@ class SSM(Base):
                     s_stds_q[i] += q.stddev.mean()
 
                 decoder_dist = Normal(*self.decoders[i](s_t))
-                x_losss[i] += - torch.sum(decoder_dist.log_prob(x_t), dim=[1,2,3]).mean()
+                x_losss[i] += - torch.sum(decoder_dist.log_prob(x_t),
+                                          dim=[1,2,3]).mean()
 
-            _x.append(decoder_dist.mean)
+            if return_x:
+                _x.append(decoder_dist.mean)
             s_prevs = s_ts
 
         if return_x:
+            _x = torch.stack(_x).transpose(0, 1)
+            _x = torch.clamp(_x, 0, 1)
             return _x
         else:
             for i in range(self.num_states):
                 if i not in self.static_hierarchy:
-                    loss += s_losss[i] + x_losss[i]  # + self.gamma * s_aux_losss[i]
+                    loss += s_losss[i] + x_losss[i]
+                    # loss += self.gamma * s_aux_losss[i]
             x_loss = x_losss[-1]
 
             _locals = locals()
@@ -166,12 +187,14 @@ class SSM(Base):
 
     def sample_s_0(self, x_0):
         _B = x_0.size(0)
-        s_prevs = [torch.zeros(_B, s_dim).to(self.device) for s_dim in self.s_dims]
+        s_prevs = [torch.zeros(_B, s_dim).to(self.device)
+                   for s_dim in self.s_dims]
         s_ts = []
         for i in range(self.num_states):
             h_t = self.encoders[i](x_0)
             s_prev = s_prevs[i]
-            a_list = [torch.zeros(_B, d).to(self.device) for d in [self.a_dim] + self.s_dims[i-1:i]]
+            a_list = [torch.zeros(_B, d).to(self.device)
+                      for d in [self.a_dim] + self.s_dims[i-1:i]]
             with torch.no_grad():
                 q = Normal(*self.posteriors[i](s_prev, h_t, a_list))
                 s_t = q.mean
@@ -181,12 +204,4 @@ class SSM(Base):
     def sample_x(self, x_0, x, a):
         with torch.no_grad():
             _x = self.forward(x_0, x, a, False, return_x=True)
-        x = x.transpose(0, 1)  # BxT
-        _x = torch.stack(_x).transpose(0, 1)  # BxT
-        _x = torch.clamp(_x, 0, 1)
-        video = []
-        for i in range(4):
-            video.append(x[i*8:i*8+8])
-            video.append(_x[i*8:i*8+8])
-        video = torch.cat(video)  # B*2xT
-        return video
+        return x, _x
